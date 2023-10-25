cc6da4227f6d7d74e829725bf89115b81f5d6786
haryuuno21 <d.shilenok@mail.ru>
2023-10-25 15:08:01 +0300

    lab2

---
+++ Lab 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle(10, 2);
+            Rectangle rectangle2 = new Rectangle(5, 8);
+            Square square = new Square(4);
+            Circle circle = new Circle(1);
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(rectangle1);
+            arrayList.Add(rectangle2);
+            arrayList.Add(square);
+            arrayList.Add(circle);
+            foreach (Shape shape in arrayList) { shape.Print(); }
+        }
+    }
+}
